6468865efd5661e58c2b2aa5012a838439d79224
Taicanium <SirLevalioDaBurnshire@gmail.com>
2024-10-21 00:37:12 -0500

    TcpPort = 5192

--- Network.cs
+++ Network.cs
@@ -22,7 +22,7 @@ namespace SylverInk
 
 		public static List<char> CodeValues { get; } = Enumerable.Range(48, 10).Concat(Enumerable.Range(65, 26)).Concat(Enumerable.Range(97, 26)).Concat([33, 35, 36, 37]).Select(c => (char)c).ToList();
 		public static Dictionary<int, int> ValueCodes { get; } = new(CodeValues.Select((c, i) => new KeyValuePair<int, int>(c, i)));
-		public static int TcpPort { get; } = 80;
+		public static int TcpPort { get; } = 5192;
 
 		public static string CodeFromAddress(IPAddress? Address, byte? Flags)
 		{
