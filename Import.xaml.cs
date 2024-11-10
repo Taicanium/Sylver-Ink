a32a72906463c500818d40968aae22c1915dc72b
Taicanium <SirLevalioDaBurnshire@gmail.com>
2024-11-09 19:47:09 -0600

    First attempt at adaptive note detection
    Log dumps from e.g. Discord and Skype will no longer be thrown off by the user sending a message with a million line breaks.
    
    IF this works right.

--- Import.xaml.cs
+++ Import.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -11,6 +13,8 @@ namespace SylverInk
 {
 	public partial class Import : Window
 	{
+		private bool Adaptive = false;
+		private string AdaptivePredicate = string.Empty;
 		private List<string> DataLines { get; } = [];
 		private int Imported = 0;
 		private BackgroundWorker? MeasureTask;
@@ -22,6 +26,18 @@ namespace SylverInk
 		{
 			InitializeComponent();
 			DataContext = Common.Settings;
+		}
+
+		private void AdaptiveChecked(object sender, RoutedEventArgs e)
+		{
+			Adaptive = AdaptiveCheckBox.IsChecked is true;
+			if (Adaptive)
+			{
+				LTPanel.IsEnabled = false;
+				DoMeasureTask();
+				return;
+			}
+			LTPanel.IsEnabled = true;
 		}
 
 		private void CloseClick(object sender, RoutedEventArgs e) => Close();
@@ -109,6 +125,97 @@ namespace SylverInk
 			if (Target.Equals(string.Empty))
 				return;
 
+			if (Adaptive)
+			{
+				using StreamReader? fileStream = new(Target);
+				if (fileStream is null || fileStream.EndOfStream)
+				{
+					Common.Settings.ImportTarget = string.Empty;
+					Target = string.Empty;
+					return;
+				}
+
+				// Letters, numbers, spaces, and punctuation, respectively.
+				string[] classes = [@"\p{L}+", @"\p{Nd}+", @"[\p{Zs}\t]+", @"[\p{P}\p{S}]+"];
+				Dictionary<string, double> frequencies = [];
+				DataLines.Clear();
+
+				while (fileStream?.EndOfStream is false)
+				{
+					string line = fileStream?.ReadLine() ?? string.Empty;
+					DataLines.Add(line);
+				}
+
+				for (int length = 3; length <= 30; length++)
+				{
+					frequencies.Clear();
+					double total = 0.0;
+
+					try
+					{
+						foreach (string key in DataLines)
+						{
+							total++;
+
+							string pattern = string.Empty;
+							for (int c = 0; c < Math.Max(0, Math.Min(key.Length, length)); c++)
+								foreach (string type in classes)
+									if (!pattern.EndsWith(type) && Regex.IsMatch(key.AsSpan(c, 1), type))
+										pattern += type;
+
+							if (!pattern.Trim().Equals(string.Empty) && !frequencies.TryAdd(pattern, 1.0))
+								frequencies[pattern] += 1.0;
+						}
+					}
+					catch
+					{
+						continue;
+					}
+
+					foreach (string key in frequencies.Keys)
+						frequencies[key] /= total;
+
+					var ordered = frequencies.OrderByDescending(pair => pair.Value);
+
+					// tl;dr: We search for note boundaries based on certain strings of characters appearing much more frequently than others at the start of lines.
+					// Think timestamps, for instance.
+					// And to be exact, we're looking for sequences that occur in at least 5% of all lines.
+					if (ordered.First().Value >= 0.05)
+						AdaptivePredicate = "^" + ordered.First().Key;
+				}
+
+				if (!AdaptivePredicate.Trim().Equals(string.Empty))
+				{
+					string recordData = string.Empty;
+					RunningAverage = 0.0;
+					RunningCount = 0;
+
+					for (int i = 0; i < DataLines.Count; i++)
+					{
+						var line = DataLines[i];
+						if (Regex.IsMatch(line, AdaptivePredicate))
+						{
+							if (!recordData.Trim().Equals(string.Empty))
+							{
+								RunningAverage += recordData.Length;
+								RunningCount++;
+							}
+							recordData = line;
+						}
+						else
+							recordData += "\r\n" + line;
+					}
+
+					RunningAverage /= RunningCount;
+					return;
+				}
+
+				MessageBox.Show("Failed to autodetect the note format.", "Sylver Ink: Error", MessageBoxButton.OK);
+				Adaptive = false;
+				AdaptiveCheckBox.IsChecked = false;
+				AdaptivePredicate = string.Empty;
+			}
+
 			try
 			{
 				using StreamReader? fileStream = new(Target);
@@ -150,7 +257,6 @@ namespace SylverInk
 			catch
 			{
 				MessageBox.Show($"Could not open file: {Target}", "Sylver Ink: Error", MessageBoxButton.OK, MessageBoxImage.Error);
-				return;
 			}
 		}
 
@@ -198,19 +304,32 @@ namespace SylverInk
 			for (int i = 0; i < DataLines.Count; i++)
 			{
 				string line = DataLines[i];
-				recordData += line + "\r\n";
 
-				if (line.Length == 0)
-					blankCount++;
-				else
-					blankCount = 0;
-
-				if (recordData.Length > 0 && (blankCount >= Common.Settings.LineTolerance || i >= DataLines.Count - 1))
+				if (Adaptive)
 				{
-					CurrentDatabase.CreateRecord(recordData);
-					Imported++;
-					recordData = string.Empty;
-					blankCount = 0;
+					if (Regex.IsMatch(line, AdaptivePredicate) && !recordData.Trim().Equals(string.Empty))
+					{
+						CurrentDatabase.CreateRecord(recordData);
+						Imported++;
+						recordData = string.Empty;
+					}
+					recordData += line + "\r\n";
+				}
+				else
+				{
+					recordData += line + "\r\n";
+					if (line.Length == 0)
+						blankCount++;
+					else
+						blankCount = 0;
+
+					if (recordData.Length > 0 && (blankCount >= Common.Settings.LineTolerance || i >= DataLines.Count - 1))
+					{
+						CurrentDatabase.CreateRecord(recordData);
+						Imported++;
+						recordData = string.Empty;
+						blankCount = 0;
+					}
 				}
 			}
 		}
@@ -218,7 +337,7 @@ namespace SylverInk
 		private void Target_TextChanged(object sender, RoutedEventArgs e)
 		{
 			Target = Common.Settings.ImportTarget;
-			Common.Settings.ReadyToFinalize = !Common.Settings.ImportTarget.Equals(string.Empty);
+			Common.Settings.ReadyToFinalize = !Target.Equals(string.Empty);
 		}
 	}
 }
