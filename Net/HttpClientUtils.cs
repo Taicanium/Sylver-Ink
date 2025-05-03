35397c9d6a47235ed545c5400aa1034c114ec7e6
Taicanium <SirLevalioDaBurnshire@gmail.com>
2025-05-03 10:42:35 -0400

    Fixed a potential error when updating

--- Net/HttpClientUtils.cs
+++ Net/HttpClientUtils.cs
@@ -9,7 +9,7 @@ public static class HttpClientUtils
 	public static async Task DownloadFileTaskAsync(this HttpClient client, string uri, string FileName)
 	{
 		using var stream = await client.GetStreamAsync(uri);
-		using var fs = new FileStream(FileName, FileMode.CreateNew);
+		using var fs = new FileStream(FileName, FileMode.Create);
 		await stream.CopyToAsync(fs);
 	}
 }
