5da5d0b12dbd06b38cabaa0475533ebd9f6afb7b
Taicanium <SirLevalioDaBurnshire@gmail.com>
2025-05-26 23:49:55 -0400

    Top-level namespace statements

--- PopupUtils.cs
+++ PopupUtils.cs
@@ -4,69 +4,68 @@ using System.Windows;
 using System.Windows.Input;
 using static SylverInk.Common;
 
-namespace SylverInk
+namespace SylverInk;
+
+public static class PopupUtils
 {
-	public static class PopupUtils
+	public static void Popup_AddressKeyDown(this MainWindow window, object? sender, KeyEventArgs e)
 	{
-		public static void Popup_AddressKeyDown(this MainWindow window, object? sender, KeyEventArgs e)
-		{
-			if (e.Key == Key.Enter)
-				window.ConnectAddress.IsOpen = false;
-		}
-
-		public static void Popup_CodeClosed(this MainWindow window, object? sender, EventArgs e) => Clipboard.SetText(window.CodeBox.Text);
-
-		public static void Popup_CopyCode(this MainWindow window, object? sender, RoutedEventArgs e) => window.CodePopup.IsOpen = false;
-
-		public static void Popup_RenameClosed(this MainWindow window, object? sender, EventArgs e)
-		{
-			if (!window.RenameDatabase.IsOpen)
-				return;
-
-			window.RenameDatabase.IsOpen = false;
-
-			if (string.IsNullOrWhiteSpace(window.DatabaseNameBox.Text))
-				return;
-
-			if (window.DatabaseNameBox.Text.Equals(CurrentDatabase.Name))
-				return;
-
-			foreach (Database db in Databases)
-			{
-				if (!window.DatabaseNameBox.Text.Equals(db.Name))
-					continue;
-
-				MessageBox.Show("A database already exists with the provided name.", "Sylver Ink: Error", MessageBoxButton.OK, MessageBoxImage.Error);
-				return;
-			}
-
-			foreach (char pc in InvalidPathChars)
-			{
-				if (window.DatabaseNameBox.Text.Contains(pc))
-				{
-					MessageBox.Show($"Provided name contains invalid character: {pc}", "Sylver Ink: Error", MessageBoxButton.OK, MessageBoxImage.Error);
-					return;
-				}
-			}
-
-			CurrentDatabase.Rename(window.DatabaseNameBox.Text);
-		}
-
-		public static void Popup_RenameKeyDown(this MainWindow window, object? sender, KeyEventArgs e)
-		{
-			if (e.Key == Key.Enter)
-				window.RenameDatabase.IsOpen = false;
-		}
-
-		public static async void Popup_SaveAddress(this MainWindow window, object? sender, RoutedEventArgs e)
-		{
+		if (e.Key == Key.Enter)
 			window.ConnectAddress.IsOpen = false;
+	}
 
-			Database newDB = new();
-			AddDatabase(newDB);
+	public static void Popup_CodeClosed(this MainWindow window, object? sender, EventArgs e) => Clipboard.SetText(window.CodeBox.Text);
 
-			var addr = window.AddressBox.Text;
-			await newDB.Client.Connect(addr);
+	public static void Popup_CopyCode(this MainWindow window, object? sender, RoutedEventArgs e) => window.CodePopup.IsOpen = false;
+
+	public static void Popup_RenameClosed(this MainWindow window, object? sender, EventArgs e)
+	{
+		if (!window.RenameDatabase.IsOpen)
+			return;
+
+		window.RenameDatabase.IsOpen = false;
+
+		if (string.IsNullOrWhiteSpace(window.DatabaseNameBox.Text))
+			return;
+
+		if (window.DatabaseNameBox.Text.Equals(CurrentDatabase.Name))
+			return;
+
+		foreach (Database db in Databases)
+		{
+			if (!window.DatabaseNameBox.Text.Equals(db.Name))
+				continue;
+
+			MessageBox.Show("A database already exists with the provided name.", "Sylver Ink: Error", MessageBoxButton.OK, MessageBoxImage.Error);
+			return;
 		}
+
+		foreach (char pc in InvalidPathChars)
+		{
+			if (window.DatabaseNameBox.Text.Contains(pc))
+			{
+				MessageBox.Show($"Provided name contains invalid character: {pc}", "Sylver Ink: Error", MessageBoxButton.OK, MessageBoxImage.Error);
+				return;
+			}
+		}
+
+		CurrentDatabase.Rename(window.DatabaseNameBox.Text);
+	}
+
+	public static void Popup_RenameKeyDown(this MainWindow window, object? sender, KeyEventArgs e)
+	{
+		if (e.Key == Key.Enter)
+			window.RenameDatabase.IsOpen = false;
+	}
+
+	public static async void Popup_SaveAddress(this MainWindow window, object? sender, RoutedEventArgs e)
+	{
+		window.ConnectAddress.IsOpen = false;
+
+		Database newDB = new();
+		AddDatabase(newDB);
+
+		var addr = window.AddressBox.Text;
+		await newDB.Client.Connect(addr);
 	}
 }
--- XAMLUtils/SearchUtils.cs
+++ XAMLUtils/SearchUtils.cs
@@ -4,52 +4,51 @@ using System.Threading.Tasks;
 using System.Windows.Documents;
 using System.Windows.Markup;
 
-namespace SylverInk.XAMLUtils
+namespace SylverInk.XAMLUtils;
+
+public static class SearchUtils
 {
-	public static class SearchUtils
+	public static async Task SearchDatabase(this Search window, Database db)
 	{
-		public static async Task SearchDatabase(this Search window, Database db)
+		db.UpdateWordPercentages();
+
+		for (int i = 0; i < db.RecordCount; i++)
 		{
-			db.UpdateWordPercentages();
+			var newRecord = db.GetRecord(i);
 
-			for (int i = 0; i < db.RecordCount; i++)
-			{
-				var newRecord = db.GetRecord(i);
+			if (newRecord is null)
+				continue;
 
-				if (newRecord is null)
-					continue;
+			bool textFound = await SearchRecord(window, newRecord);
 
-				bool textFound = await SearchRecord(window, newRecord);
+			if (!textFound)
+				continue;
 
-				if (!textFound)
-					continue;
+			window.ResultsList.Add(newRecord);
+			window.DBMatches.TryAdd(newRecord, db);
+		}
+	}
 
-				window.ResultsList.Add(newRecord);
-				window.DBMatches.TryAdd(newRecord, db);
-			}
+	private static async Task<bool> SearchRecord(this Search window, NoteRecord record) => await Task.Run(() =>
+	{
+		var document = (FlowDocument)XamlReader.Parse(record.ToXaml());
+		TextPointer? pointer = document.ContentStart;
+		while (pointer is not null && pointer.GetPointerContext(LogicalDirection.Forward) != TextPointerContext.None)
+		{
+			while (pointer is not null && pointer.GetPointerContext(LogicalDirection.Forward) != TextPointerContext.Text)
+				pointer = pointer.GetNextContextPosition(LogicalDirection.Forward);
+
+			if (pointer is null)
+				break;
+
+			string recordText = pointer.GetTextInRun(LogicalDirection.Forward);
+			if (recordText.Contains(window.Query, StringComparison.OrdinalIgnoreCase))
+				return true;
+
+			while (pointer.GetPointerContext(LogicalDirection.Forward) == TextPointerContext.Text)
+				pointer = pointer.GetNextContextPosition(LogicalDirection.Forward);
 		}
 
-		private static async Task<bool> SearchRecord(this Search window, NoteRecord record) => await Task.Run(() =>
-		{
-			var document = (FlowDocument)XamlReader.Parse(record.ToXaml());
-			TextPointer? pointer = document.ContentStart;
-			while (pointer is not null && pointer.GetPointerContext(LogicalDirection.Forward) != TextPointerContext.None)
-			{
-				while (pointer is not null && pointer.GetPointerContext(LogicalDirection.Forward) != TextPointerContext.Text)
-					pointer = pointer.GetNextContextPosition(LogicalDirection.Forward);
-
-				if (pointer is null)
-					break;
-
-				string recordText = pointer.GetTextInRun(LogicalDirection.Forward);
-				if (recordText.Contains(window.Query, StringComparison.OrdinalIgnoreCase))
-					return true;
-
-				while (pointer.GetPointerContext(LogicalDirection.Forward) == TextPointerContext.Text)
-					pointer = pointer.GetNextContextPosition(LogicalDirection.Forward);
-			}
-
-			return false;
-		});
-	}
+		return false;
+	});
 }
